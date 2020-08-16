d86c3761f9122c99179dd5873f04f2f8314ddb8c
aslozada <aslozada@gmail.com>
2020-08-16 16:53:45 -0300

    Initialize module: mod_read_table

---
+++ src/mod_read_table.f90
@@ -0,0 +1,35 @@
+!---------------------------------------------------------------------------------------------------
+! THEMIS: A code to study intermolecular recognition via direct partition function estimation                                                  
+!---------------------------------------------------------------------------------------------------
+!
+!   Free software, licensed under GNU GPL v3
+!
+!   Copyright (c) 2017 - 2020 Themis developers
+!                 Laboratory of Theoretical Chemistry (LQT) - Federal University of São Carlos 
+!                 <http://www.lqt.dq.ufscar.br>
+!
+!   Please cite: 
+!
+!   This file was written by Felippe M. Colombari and Asdrubal Lozada-Blanco.
+!
+!---------------------------------------------------------------------------------------------------
+!
+!   Themis is free software: you can redistribute it and/or modify it under the terms of the GNU 
+!   General Public License as published by the Free Software Foundation, either version 3 of the 
+!   License, or (at your option) any later version.
+!
+!   This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+!   without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See 
+!   the GNU General Public License for more details.
+!
+!   You should have received a copy of the GNU General Public License along with this program. If 
+!   not, see <https://www.gnu.org/licenses/>.
+!
+!---------------------------------------------------------------------------------------------------
+!> @file   mod_read_table.f90
+!> @author Asdrubal Lozada-Blanco
+!> @brief  This module reads a tabulated potential
+!> @date - Aug, 2020                                                          
+!------------------------------------------------------------------------------
+
+! @TODO
