c3b3198748b31343292e238ac9c3be1267f6cb41
felippe <colombarifm@hotmail.com>
2021-01-29 06:52:09 -0300

    update utils

---
+++ build/CMakeFiles/themis.dir/cmake_clean_Fortran.cmake
@@ -0,0 +1,70 @@
+# Remove fortran modules provided by this target.
+FILE(REMOVE
+  "mod_cmd_line.mod"
+  "MOD_CMD_LINE.mod"
+  "CMakeFiles/themis.dir/mod_cmd_line.mod.stamp"
+
+  "mod_constants.mod"
+  "MOD_CONSTANTS.mod"
+  "CMakeFiles/themis.dir/mod_constants.mod.stamp"
+
+  "mod_deallocate_all.mod"
+  "MOD_DEALLOCATE_ALL.mod"
+  "CMakeFiles/themis.dir/mod_deallocate_all.mod.stamp"
+
+  "mod_error_handling.mod"
+  "MOD_ERROR_HANDLING.mod"
+  "CMakeFiles/themis.dir/mod_error_handling.mod.stamp"
+
+  "mod_grids.mod"
+  "MOD_GRIDS.mod"
+  "CMakeFiles/themis.dir/mod_grids.mod.stamp"
+
+  "mod_info.mod"
+  "MOD_INFO.mod"
+  "CMakeFiles/themis.dir/mod_info.mod.stamp"
+
+  "mod_input_read.mod"
+  "MOD_INPUT_READ.mod"
+  "CMakeFiles/themis.dir/mod_input_read.mod.stamp"
+
+  "mod_inquire.mod"
+  "MOD_INQUIRE.mod"
+  "CMakeFiles/themis.dir/mod_inquire.mod.stamp"
+
+  "mod_loops.mod"
+  "MOD_LOOPS.mod"
+  "CMakeFiles/themis.dir/mod_loops.mod.stamp"
+
+  "mod_pot_bhc.mod"
+  "MOD_POT_BHC.mod"
+  "CMakeFiles/themis.dir/mod_pot_bhc.mod.stamp"
+
+  "mod_pot_ljc.mod"
+  "MOD_POT_LJC.mod"
+  "CMakeFiles/themis.dir/mod_pot_ljc.mod.stamp"
+
+  "mod_read_molecules.mod"
+  "MOD_READ_MOLECULES.mod"
+  "CMakeFiles/themis.dir/mod_read_molecules.mod.stamp"
+
+  "mod_resume.mod"
+  "MOD_RESUME.mod"
+  "CMakeFiles/themis.dir/mod_resume.mod.stamp"
+
+  "mod_search_structures.mod"
+  "MOD_SEARCH_STRUCTURES.mod"
+  "CMakeFiles/themis.dir/mod_search_structures.mod.stamp"
+
+  "mod_spherical_grids.mod"
+  "MOD_SPHERICAL_GRIDS.mod"
+  "CMakeFiles/themis.dir/mod_spherical_grids.mod.stamp"
+
+  "mod_write_vmd.mod"
+  "MOD_WRITE_VMD.mod"
+  "CMakeFiles/themis.dir/mod_write_vmd.mod.stamp"
+
+  "xdr.mod"
+  "XDR.mod"
+  "CMakeFiles/themis.dir/xdr.mod.stamp"
+  )
