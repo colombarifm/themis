966a869055c38612a20a48fdbf5d612ad36336a0
Asdrubal Lozada-Blanco <aslozada@gmail.com>
2023-11-09 22:59:38 -0300

    Create conf.py

---
+++ source/conf.py
@@ -0,0 +1,20 @@
+project = 'themis'
+copyright = '2023 Themis developers'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+root_doc = 'index'
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+~                               
